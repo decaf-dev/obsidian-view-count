2519a4fa126c2b58dcb2835fa2e5c9e4b25c4710
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2024-05-24 20:09:05 -0600

    fix: resolve infinite increase on click

--- src/obsidian/view-count-item-view.ts
+++ src/obsidian/view-count-item-view.ts
@@ -61,7 +61,7 @@ export default class ViewCountItemView extends ItemView {
 				const type = (this.app as any).viewRegistry.getTypeByExtension(file.extension);
 				this.app.workspace.getLeaf(false)?.setViewState({
 					type,
-					active: true,
+					active: false,
 					state: {
 						file: entry.path,
 					},
