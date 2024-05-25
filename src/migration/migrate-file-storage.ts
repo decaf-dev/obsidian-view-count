27fe8a7ef9cf1990898be78b55579b095e55ea0b
Trey Wallis <40307803+trey-wallis@users.noreply.github.com>
2024-05-24 18:23:52 -0600

    fix: add open log entry from last view date

--- src/migration/migrate-file-storage.ts
+++ src/migration/migrate-file-storage.ts
@@ -4,27 +4,29 @@ import { ViewCountEntry, ViewCountEntry_1_2_2 } from "src/storage/types";
 import { getFilePath, parseEntries, stringifyEntries } from "src/storage/utils";
 import { ViewCountPluginSettings_1_2_2 } from "src/types";
 
-export default class MigrateFileStorage {
-	async migrateTo2_0_0(app: App, settings: ViewCountPluginSettings_1_2_2) {
-		const path = getFilePath(app);
-		const exists = await app.vault.adapter.exists(path);
-		if (exists) {
-			Logger.info("Migrating view count file storage from 1.2.2 to 2.0.0");
-			const fileData = await app.vault.adapter.read(path);
-			const entries = parseEntries<ViewCountEntry_1_2_2>(fileData);
+export const migrateFileStorage = async (app: App, settings: ViewCountPluginSettings_1_2_2) => {
+	const path = getFilePath(app);
+	const exists = await app.vault.adapter.exists(path);
+	if (exists) {
+		Logger.info("Migrating file storage from 1.2.2 to 2.0.0");
+		const fileData = await app.vault.adapter.read(path);
+		const entries = parseEntries<ViewCountEntry_1_2_2>(fileData);
 
-			const { incrementOnceADay } = settings;
-			const newEntries: ViewCountEntry[] = entries.map(entry => {
-				return {
-					path: entry.path,
-					uniqueDaysOpened: incrementOnceADay ? entry.viewCount : 0,
-					totalTimesOpened: entry.viewCount,
-					openLogs: []
-				}
-			});
-			const data = stringifyEntries(newEntries);
-			await app.vault.adapter.write(path, data);
-			Logger.info("Migration complete");
-		}
+		const { incrementOnceADay } = settings;
+		const newEntries: ViewCountEntry[] = entries.map(entry => {
+			return {
+				path: entry.path,
+				uniqueDaysOpened: incrementOnceADay ? entry.viewCount : 0,
+				totalTimesOpened: entry.viewCount,
+				openLogs: [
+					{
+						timestampMillis: entry.lastViewMillis
+					}
+				]
+			}
+		});
+		const data = stringifyEntries(newEntries);
+		await app.vault.adapter.write(path, data);
+		Logger.info("Migration complete");
 	}
 }
